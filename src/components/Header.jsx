7a5d49af9d32fdbce4fe058ded4c6732fc32fb3d
haeun104 <haeun104@gmail.com>
2024-02-03 20:58:46 +0100

    Created header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,48 @@
 function Header() {
-  return <h1>Hello World!</h1>;
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-main">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          FitFlow <span>Planner</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Main page
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Add Plan
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Update Plan
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Today's News
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
