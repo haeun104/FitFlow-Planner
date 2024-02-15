4a191ebb1afd6b7c539ce801e5e1771447a89551
haeun104 <haeun104@gmail.com>
2024-02-15 19:28:20 +0100

    Amended the summary list on main page

--- src/components/MainSummaryList.jsx
+++ src/components/MainSummaryList.jsx
@@ -9,6 +9,14 @@ const MainSummaryList = () => {
     navigate("/update");
   };
 
+  const today = new Date();
+  const nextWeek = today.setDate(today.getDate() + 7);
+  const nextWeekTimestamp = new Date(nextWeek).getTime();
+
+  const nextWeekList = summarizedList.filter(
+    (item) => new Date(item.date) < nextWeekTimestamp
+  );
+
   return (
     <div className="main-summary">
       <div className="main-summary-title">
@@ -17,7 +25,7 @@ const MainSummaryList = () => {
           Go to details
         </button>
       </div>
-      {summarizedList.map((item, index) => (
+      {nextWeekList.map((item, index) => (
         <ul key={index} className="summary-list">
           <li className="summary-list-date">{item.date}</li>
           <li className="summary-list-category">{item.category.join(" | ")}</li>
